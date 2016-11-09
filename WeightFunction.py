d8b36be20efb8810443bc8ac0ed3832d8119ae36
Pablo <jpdominguez94@gmail.com>
2016-11-09 02:25:14 -0600

    Adding Weight Function

---
+++ WeightFunction.py
@@ -0,0 +1,10 @@
+def zen_score(weights, values):
+    mult_lists = [a * b for a, b in zip(weights, values)]
+    total = sum(i for i in mult_lists)
+    return total
+
+list_weight = [1, 2, 3]
+list_values = [1, 3, 4]
+result = zen_score(list_weight, list_values)
+
+print "The weight is: ", result
