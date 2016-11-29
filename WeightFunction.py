5f534c633a6c30c2820184e463d77a6a0a2cec29
Justin Lewis <justin94lewis@yahoo.com>
2016-11-29 11:20:20 -0600

    added weight function and weight function example

---
+++ WeightFunction.py
@@ -0,0 +1,22 @@
+__author__ = 'Justin'
+
+def zen_score(weights,edge_dictionary):
+    keys = ['traffic','time','distance']
+    values=[]
+
+    for key in keys:
+        values.append(edge_dictionary[key])
+
+    mult_lists = [a * b for a, b in zip(weights, values)]       #Multiply both lists
+    total = sum(i for i in mult_lists)       #Add lists
+    return total
+
+"""TEST
+
+list_weight = [1, 2, 3]
+list_values = [1, 3, 3]
+result = zen_score(list_weight, list_values)
+
+print "The weight is: ", result
+
+"""
