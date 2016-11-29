1dcfccebce65bd9858e3870bf07f84cb72a6f1ff
jpdominguez94 <jpdominguez94@tamu.edu>
2016-11-29 10:44:19 -0600

    Update WeightFunction.py

--- WeightFunction.py
+++ WeightFunction.py
@@ -13,13 +13,3 @@ def zen_score(weights,edge_dictionary):
     mult_lists = [a * b for a, b in zip(weights, values)]       #multiply both lists
     total = sum(i for i in mult_lists)       #add lists
     return total
-
-"""TEST
-
-list_weight = [1, 2, 3]
-list_values = [1, 3, 3]
-result = zen_score(list_weight, list_values)
-
-print "The weight is: ", result
-
-"""
