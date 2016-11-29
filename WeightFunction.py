e12086892d497a4d2d422ef801496f1efabb18be
jpdominguez94 <jpdominguez94@tamu.edu>
2016-11-29 09:51:01 -0600

    Update WeightFunction.py

--- WeightFunction.py
+++ WeightFunction.py
@@ -1,6 +1,6 @@
 def zen_score(weights, values):
-    mult_lists = [a * b for a, b in zip(weights, values)]  //Multiply both lists
-    total = sum(i for i in mult_lists)  //Add lists
+    mult_lists = [a * b for a, b in zip(weights, values)]       //Multiply both lists
+    total = sum(i for i in mult_lists)       //Add lists
     return total
 
 list_weight = [1, 2, 3]
